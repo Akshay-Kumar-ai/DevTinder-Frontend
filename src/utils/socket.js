8fb085b8f27a5691724f09a891ac3f676861399e
Akshay-Kumar-ai <akssm2002@gmail.com>
2025-07-25 22:31:47 +0530

    Socket Connection Updated for local & server

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,14 @@
 import io from "socket.io-client";
 import { Base_Url } from "./constants";
 
+// export const createSocketConnection = () => {
+//   return io(Base_Url);
+// };
+
 export const createSocketConnection = () => {
-  return io(Base_Url);
+  if (location.hostname === "localhost") {
+    return io(Base_Url);
+  } else {
+    return io("/", { path: "/api/socket.io" });
+  }
 };
